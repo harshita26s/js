72f519d93f173e689bc33d905fbe504526076b9b
harshita26s <145872843+harshita26s@users.noreply.github.com>
2023-11-15 18:28:31 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33abc"
+//let score = "null"
+//let score = null
+//let score = undefined
+//let score = true
+//let score = false
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); // number
+//console.log(valueInNumber); //NaN = not a number
+
+/*
+"33"=> 33
+ "33abc" => NaN
+ true => 1; false => 0
+*/
+
+//let isLoggedIn = 0        //false   
+//let isLoggedIn = ""       //false
+// let isLoggedIn = "hdhd"   //true
+//let isLoggedIn = 1        //false
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
