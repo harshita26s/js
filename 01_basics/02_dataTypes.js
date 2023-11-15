943d711ee728a9d7723c4508ec0a2c8108e574fe
harshita26s <145872843+harshita26s@users.noreply.github.com>
2023-11-15 17:56:15 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3) we are using nodejs, not browser 
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("harshita")
+
+let name = "harshita"
+let age = 20
+let isLoggedIn = true
+
+// number => 2 topower of 53
+//bigint 
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => value not assign
+// symbol => unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof "harshita");
+console.log(typeof null); // null is a object or it is a language error
